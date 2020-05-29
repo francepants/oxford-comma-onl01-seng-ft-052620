e1d20ad458a5e6af3d610d9bda2d5bbddbcc4ab5
Franceline Casanas <franceline90@gmail.com>
2020-05-29 02:18:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
 
 end
+
+
+# returns a string without any additional fomatting when given a 1-element array (FAILED - 1)
+# adds 'and' between elements when given a 2-element array (FAILED - 2)
+# adds commas plus a final 'and' when given a 3-element array (FAILED - 3)
+# correctly formats arrays of lengths greater than three (FAILED - 4)
