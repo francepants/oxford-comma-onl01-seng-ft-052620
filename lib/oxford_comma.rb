77e364705aa1e6ce4b375e36af1a6af37f7508f9
Franceline Casanas <franceline90@gmail.com>
2020-05-29 02:32:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,10 @@ def oxford_comma(array)
   # binding.pry
   if array.length == 1
     array[0]
-  elsif array.length == 1
+  elsif array.length == 2
     array[0] + " and " + array[1]
+  elsif array
+
   end
 end
 
