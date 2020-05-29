fcfc45f0e37085cd3c9a374b007848421174724f
Franceline Casanas <franceline90@gmail.com>
2020-05-29 02:27:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,11 @@
 require 'pry'
 
 def oxford_comma(array)
-  if array.length == 1
-    array.join
-  elsif array.length == 2
-    array.join( "and" )
+  binding.pry
+  # if array.length == 1
+  #   array.join
+  # elsif array.length == 2
+  #   array.join( "and" )
 end
 
 
